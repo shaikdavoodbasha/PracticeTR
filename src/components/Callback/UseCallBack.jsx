c9d2a9b8fe44aa271fcc8f5b34b8cbf4bc19d646
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-18 12:13:33 +0530

    c3

--- src/components/Callback/UseCallBack.jsx
+++ src/components/Callback/UseCallBack.jsx
@@ -1,23 +1,23 @@
-// import React, { useCallback, useState } from "react";
-// import Child from "./Child";
+import React, { useCallback, useState } from "react";
+import Child from "./Child";
 
-// const UseCallBack = () => {
-//   const [data, setData] = useState(0);
-//   const viewing = useCallback(() => {
-//     console.log("Helo bro i am Good");
-//   },[]);
-//   return (
-//     <div>
-//       <Child  viewing={viewing} data={data} />
-//       <button
-//         className="px-4 py-2 bg-red-600 text-white rounded-lg"
-//         onClick={() => setData(data + 1)}
-//       >
-//         Inc
-//       </button>
-//       <p>{data}</p>
-//     </div>
-//   );
-// };
+const UseCallBack = () => {
+  const [data, setData] = useState(0);
+  const viewing = useCallback(() => {
+    console.log("Helo bro i am Good");
+  },[]);
+  return (
+    <div>
+      <Child  viewing={viewing} data={data} />
+      <button
+        className="px-4 py-2 bg-red-600 text-white rounded-lg"
+        onClick={() => setData(data + 1)}
+      >
+        Inc
+      </button>
+      <p>{data}</p>
+    </div>
+  );
+};
 
-// export default UseCallBack;
+export default UseCallBack;

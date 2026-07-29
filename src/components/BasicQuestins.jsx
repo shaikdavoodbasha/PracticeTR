30574d5c306b9076f1ea8699a63dbdaefdbba462
shaikdavoodbasha <shannushaik080@gmail.com>
2026-07-29 11:07:04 +0530

    lkoim

--- src/components/BasicQuestins.jsx
+++ src/components/BasicQuestins.jsx
@@ -20,9 +20,6 @@ const BasicQuestins = ({ data }) => {
         <p>{data.class}</p>
         <p>{data.section}</p>        
       </div>
-
-
-      
       <div className="flex gap-3 py-6">
         <button
           className="px-3 py-1 bg-red-600 text-white"

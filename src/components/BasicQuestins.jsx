ba059799bd98f685ed6249aa4185bc98f90613a4
shaikdavoodbasha <shannushaik080@gmail.com>
2026-08-05 08:41:44 +0530

    lkjhg

--- src/components/BasicQuestins.jsx
+++ src/components/BasicQuestins.jsx
@@ -16,7 +16,6 @@ const BasicQuestins = ({ data }) => {
           IncVan
         </button>
       <p className="text-red-700">{name1}</p>
-
       <div>
         <p>{data.class}</p>
         <p>{data.section}</p>        

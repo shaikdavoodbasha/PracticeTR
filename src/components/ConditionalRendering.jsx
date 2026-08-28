923630e6880261a8e53a7a7cb55fb8c28f001499
shaikdavoodbasha <shannushaik080@gmail.com>
2026-08-28 22:19:56 +0530

    tyuiop

--- src/components/ConditionalRendering.jsx
+++ src/components/ConditionalRendering.jsx
@@ -8,7 +8,7 @@ const ConditionalRendering = ({ sendToParent }) => {
   };
   return van ? (
     <div>
-      <h1>Hii I am Conditionkjhaly Render</h1>bnjkop
+      <h1>Hii I am Conditionkjhaly Render</h1>
     <button
         className="px-3 py-2 bg-black text-white rounded-xl"
         onClick={action}
